3e56b5b9072c88335a3ecb2f6d6e767140b79e62
David Ngacha <davidngacha01@gmail.com>
2025-12-23 18:27:33 +0300

    black navigation bar

--- app/(pages)/contact/page.jsx
+++ app/(pages)/contact/page.jsx
@@ -1,5 +1,6 @@
 import Address from '@/app/components/contact/Address'
 import ContactHero from '@/app/components/contact/ContactHero'
+import BlackNavbar from '@/app/components/global/BlackNavBar'
 import Contact from '@/app/components/global/Contact'
 import Navbar from '@/app/components/global/Navbar'
 import React from 'react'
@@ -7,7 +8,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-        <Navbar />
+        <BlackNavbar />
         <ContactHero />
         <Address />
         <Contact />
