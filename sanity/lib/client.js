791b9f59a93e26aea06f3c166cab8a2e6c45adb0
David Ngacha <davidngacha01@gmail.com>
2025-12-30 00:28:39 +0300

    add env fallbacks

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2024-01-01',
+  projectId,
+  dataset,
+  apiVersion,
   useCdn: true,
 })
